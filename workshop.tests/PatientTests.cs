3034ee3139dd0f0c0de2f2117b985ccd9d667764
Sander Rasmussen <sander.rasmussn@gmail.com>
2025-01-27 15:03:03 +0100

    done with core

--- workshop.tests/PatientTests.cs
+++ workshop.tests/PatientTests.cs
@@ -14,9 +14,61 @@ public class Tests
         var client = factory.CreateClient();
 
         // Act
-        var response = await client.GetAsync("/patients");
+        var response = await client.GetAsync("/surgery/patients");
 
         // Assert
-        Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
+        Assert.That(response.StatusCode==System.Net.HttpStatusCode.OK);
+    }
+    [Test]
+    public async Task DoctorsEndpointStatus()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/surgery/Doctors");
+
+        // Assert
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
+    }
+    [Test]
+    public async Task GetDoctorByIdEndpointStatus()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/surgery/Doctor/1");
+
+        // Assert
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
+    }
+    [Test]
+    public async Task appointmentsBydoctorEndpointStatus()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/surgery/appointmentsbydoctor/1");
+
+        // Assert
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
+    }
+    [Test]
+    public async Task getPatientEndpointStatus()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/surgery/patient1");
+
+        // Assert
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
     }
 }
